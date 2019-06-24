3f5faea219466b53f9f5a8ca1814c103d6500416
grzegrzk <grzegorz.kuligowski@gmail.com>
2019-06-24 20:32:37 +0200

    solution 2

--- Solution2.java
+++ Solution2.java
@@ -1,7 +1,13 @@
-public class Solution2
-{
-    public static void main(String[] args)
-    {
-        // write your code here
+public class Solution2 {
+    public static void main(String[] args) {
+        new Solution2().sumOfAscii(args[1]);
+    }
+
+    public long sumOfAscii(String arg) {
+        long toRet = 0;
+        for (int i = 0; i < arg.length(); ++i) {
+            toRet += arg.charAt(i);
+        }
+        return toRet;
     }
 }
--- tests/Solution2Test.java
+++ tests/Solution2Test.java
@@ -1,3 +1,17 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Solution2Test {
 
+    @Test
+    public void test() {
+        Solution2 sut = new Solution2();
+
+        assertThat(sut.sumOfAscii("a")).isEqualTo(97);
+        assertThat(sut.sumOfAscii("aa")).isEqualTo(97 * 2);
+        assertThat(sut.sumOfAscii("0")).isEqualTo(48);
+        assertThat(sut.sumOfAscii("1")).isEqualTo(49);
+        assertThat(sut.sumOfAscii("01")).isEqualTo(97);
+    }
 }
