7d0c7e3ea1ee9338dac36af023fbb5d632858b92
grzegrzk <grzegorz.kuligowski@gmail.com>
2019-06-24 22:29:23 +0200

    shifted back fibonacci solutions

--- Solution3.java
+++ Solution3.java
@@ -1,10 +1,10 @@
 public class Solution3 {
     public static void main(String[] args) {
-        System.out.println(new Solution3().nthFibonacci(Long.parseLong(args[0])));
+        System.out.print(new Solution3().nthFibonacci(Long.parseLong(args[0])));
     }
 
     public long nthFibonacci(long n) {
-        long prev1 = 1;
+        long prev1 = 0;
         long cur = 1;
         if (n == 0) {
             return prev1;
