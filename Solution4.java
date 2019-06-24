df87b7941c21d44a63d9c768ed1e8dffa6050142
grzegrzk <grzegorz.kuligowski@gmail.com>
2019-06-24 21:42:57 +0200

    solution 4

--- Solution4.java
+++ Solution4.java
@@ -1,7 +1,29 @@
-public class Solution4
-{
-    public static void main(String[] args)
-    {
-        // write your code here
+import java.util.List;
+
+public class Solution4 {
+    public static void main(String[] args) {
+        System.out.println(new Solution4().nthLookAndSay(Integer.parseInt(args[0])));
+    }
+
+    public String nthLookAndSay(int n) {
+        String cur = "1";
+        for (int i = 1; i < n; ++i) {
+            cur = computeNextLookAndSay(cur);
+        }
+        return cur;
+    }
+
+    public String computeNextLookAndSay(String cur) {
+        StringBuilder next = new StringBuilder();
+        for (int i = 0; i < cur.length(); ) {
+            int count = 1;
+            while (i + count < cur.length() && cur.charAt(i + count) == (cur.charAt(i))) {
+                ++count;
+            }
+            next.append(count);
+            next.append(cur.charAt(i));
+            i = i + count;
+        }
+        return next.toString();
     }
 }
---
+++ tests/Solution4Test.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Solution4Test {
+
+    @Test
+    public void test() {
+        Solution4 s4 = new Solution4();
+        assertThat(s4.nthLookAndSay(1)).isEqualTo("1");
+        assertThat(s4.nthLookAndSay(2)).isEqualTo("11");
+        assertThat(s4.nthLookAndSay(3)).isEqualTo("21");
+        assertThat(s4.nthLookAndSay(4)).isEqualTo("1211");
+        assertThat(s4.nthLookAndSay(5)).isEqualTo("111221");
+        assertThat(s4.nthLookAndSay(6)).isEqualTo("312211");
+        assertThat(s4.nthLookAndSay(7)).isEqualTo("13112221");
+        assertThat(s4.nthLookAndSay(8)).isEqualTo("1113213211");
+        assertThat(s4.nthLookAndSay(20)).isEqualTo("11131221131211132221232112111312111213111213211231132132211211131221131211221321123113213221123113112221131112311332211211131221131211132211121312211231131112311211232221121321132132211331121321231231121113112221121321133112132112312321123113112221121113122113121113123112112322111213211322211312113211");
+    }
+}
