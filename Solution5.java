4036bd3e35290c5ef555b6f437eae30decbfd714
grzegrzk <grzegorz.kuligowski@gmail.com>
2019-06-24 23:30:52 +0200

    nth vampire - shift by 1

--- Solution5.java
+++ Solution5.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class Solution5 {
     public static void main(String[] args) {
-        System.out.println(new Solution5().nthVampireNumber(Integer.parseInt(args[0])));
+        System.out.println(new Solution5().nthVampireNumber(Integer.parseInt(args[0]) + 1));
     }
 
     public long nthVampireNumber(int n) {
