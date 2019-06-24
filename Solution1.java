e9a5ca4c5cb566f13e01bc00b653c1c94eedf828
grzegrzk <grzegorz.kuligowski@gmail.com>
2019-06-24 21:02:19 +0200

    solution 1

--- Solution1.java
+++ Solution1.java
@@ -1,9 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 
-public class Solution1
-{
-    public static void main(String[] args) throws IOException
-    {
-        // write your code here
+public class Solution1 {
+    public static void main(String[] args) throws IOException {
+        Solution1 s1 = new Solution1();
+        try (Reader fr = new FileReader(args[0])) {
+            System.out.println(
+                    s1.palindromesCount(fr)
+            );
+        }
+    }
+
+    public long palindromesCount(Reader reader) throws IOException {
+        BufferedReader br = new BufferedReader(reader);
+        String line = null;
+        long palindromeCount = 0;
+        while ((line = br.readLine()) != null) {
+            String trimmed = line.trim();
+            if (!trimmed.isEmpty() && isPalindrome(trimmed)) {
+                ++palindromeCount;
+            }
+        }
+        return palindromeCount;
+    }
+
+    private boolean isPalindrome(String word) {
+        char[] chars = word.toCharArray();
+        StringBuilder reversed = new StringBuilder();
+        for (int i = 0; i < chars.length; ++i) {
+            reversed.append(chars[chars.length - i - 1]);
+        }
+        return reversed.toString().equals(word);
     }
 }
--- tests/Solution1Test.java
+++ tests/Solution1Test.java
@@ -1,5 +1,24 @@
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Solution1Test {
 
+    @Test
+    public void test() throws IOException {
+        Solution1 s1 = new Solution1();
+
+        assertThat(s1.palindromesCount(reader(""))).isEqualTo(0);
+        assertThat(s1.palindromesCount(reader("abc"))).isEqualTo(0);
+        assertThat(s1.palindromesCount(reader("abc\nnan\nuku"))).isEqualTo(2);
+    }
+
+    private Reader reader(String str) {
+        return new StringReader(str);
+    }
 
 }
