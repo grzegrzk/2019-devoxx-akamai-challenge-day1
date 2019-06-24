c17105fbc7ab13f8a18c8e19f9881e5e598436b0
grzegrzk <grzegorz.kuligowski@gmail.com>
2019-06-24 21:16:43 +0200

    solution 3

--- Solution3.java
+++ Solution3.java
@@ -1,7 +1,22 @@
-public class Solution3
-{
-    public static void main(String[] args)
-    {
-        // write your code here
+public class Solution3 {
+    public static void main(String[] args) {
+        System.out.println(new Solution3().nthFibonacci(Integer.parseInt(args[0])));
+    }
+
+    public long nthFibonacci(int n) {
+        long prev1 = 1;
+        long cur = 1;
+        if (n == 1) {
+            return prev1;
+        }
+        if (n == 2) {
+            return cur;
+        }
+        for (int i = 3; i <= n; ++i) {
+            long temp = prev1;
+            prev1 = cur;
+            cur = prev1 + temp;
+        }
+        return cur;
     }
 }
---
+++ tests/Solution3Test.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Solution3Test {
+
+    @Test
+    public void test(){
+        Solution3 s3 = new Solution3();
+
+        assertThat(s3.nthFibonacci(1)).isEqualTo(1);
+        assertThat(s3.nthFibonacci(2)).isEqualTo(1);
+        assertThat(s3.nthFibonacci(3)).isEqualTo(2);
+        assertThat(s3.nthFibonacci(4)).isEqualTo(3);
+        assertThat(s3.nthFibonacci(5)).isEqualTo(5);
+        assertThat(s3.nthFibonacci(6)).isEqualTo(8);
+        assertThat(s3.nthFibonacci(7)).isEqualTo(13);
+        assertThat(s3.nthFibonacci(8)).isEqualTo(21);
+        assertThat(s3.nthFibonacci(9)).isEqualTo(34);
+        assertThat(s3.nthFibonacci(10)).isEqualTo(55);
+    }
+}
