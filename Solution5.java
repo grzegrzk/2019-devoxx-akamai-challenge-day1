2246e401b3c8d079303c83f27bf857f4fe49c969
grzegrzk <grzegorz.kuligowski@gmail.com>
2019-06-24 23:25:56 +0200

    Solution 5

--- Solution5.java
+++ Solution5.java
@@ -1,31 +1,64 @@
+import java.math.BigDecimal;
+import java.util.Arrays;
+
 public class Solution5 {
     public static void main(String[] args) {
-//        System.out.println(new Solution5().nthVampireNumber(Integer.parseInt(args[0])));
+        System.out.println(new Solution5().nthVampireNumber(Integer.parseInt(args[0])));
     }
 
-//    private long nthVampireNumber(int n) {
-//        long curCandidate = 0;
-//        for (int i = 0; i < n; ++i) {
-//            curCandidate = findVampireNumberLargerThen(curCandidate);
-//        }
-//        return curCandidate;
-//    }
-//
-//    private long findVampireNumberLargerThen(long min) {
-//        long cur = min;
-//        while (!isVampire(cur)) {
-//            ++cur;
-//        }
-//        return cur;
-//    }
-//
-//    private boolean isVampire(long candidate) {
-//        String candidateAsString = String.valueOf(candidate);
-//        if (candidateAsString.length() % 2 == 1) {
-//            return false;
-//        }
-//        String firstHalf = candidateAsString.substring(0, candidateAsString.length() / 2);
-//        String secondHalf = candidateAsString.substring(candidateAsString.length() / 2);
-//
-//    }
+    public long nthVampireNumber(int n) {
+        long curCandidate = 0;
+        for (int i = 0; i < n; ++i) {
+            curCandidate = findVampireNumberNotSmallerThan(curCandidate + 1);
+        }
+        return curCandidate;
+    }
+
+    public long findVampireNumberNotSmallerThan(long min) {
+        long cur = min;
+        while (!isVampire(cur)) {
+            ++cur;
+        }
+        return cur;
+    }
+
+    public boolean isVampire(long candidate) {
+        String candidateAsString = String.valueOf(candidate);
+        if (candidateAsString.length() % 2 == 1) {
+            return false;
+        }
+        int[] sortedOriginalDigits = getSortedDigits(candidateAsString);
+        int factorizedLength = candidateAsString.length() / 2;
+        long minFactorized = BigDecimal.ONE.movePointRight(factorizedLength - 1).longValue();
+        long maxFactorized = minFactorized * 10 - 1;
+        for (long candidateFactor = minFactorized; candidateFactor <= maxFactorized; ++candidateFactor) {
+            long div = candidate / candidateFactor;
+            long mod = candidate % candidateFactor;
+            if (mod == 0 && (div >= minFactorized && div <= maxFactorized)) {
+                if (
+                        (candidateFactor % 10 > 0 || div % 10 > 0)
+                                && Arrays.equals(
+                                sortedOriginalDigits,
+                                getSortedDigits(String.valueOf(candidateFactor) + String.valueOf(div))
+                        )
+                ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private int[] getSortedDigits(String string) {
+        int[] digits = new int[string.length()];
+        long num = Long.parseLong(string);
+        int i = 0;
+        while (num > 0) {
+            digits[i++] = (int) (num % 10);
+            num = num / 10;
+        }
+        Arrays.sort(digits);
+        return digits;
+    }
 }
+
---
+++ tests/Solution5Test.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Solution5Test {
+
+    @Test
+    public void test() {
+        Solution5 s5 = new Solution5();
+
+        assertThat(s5.isVampire(1259)).isEqualTo(false);
+        assertThat(s5.isVampire(1260)).isEqualTo(true);
+        assertThat(s5.isVampire(1261)).isEqualTo(false);
+        assertThat(s5.isVampire(126000)).isEqualTo(false);
+    }
+
+    @Test
+    public void testNth(){
+        Solution5 s5 = new Solution5();
+
+        assertThat(s5.nthVampireNumber(1)).isEqualTo(1260);
+        assertThat(s5.nthVampireNumber(2)).isEqualTo(1395);
+        assertThat(s5.nthVampireNumber(3)).isEqualTo(1435);
+        assertThat(s5.nthVampireNumber(4)).isEqualTo(1530);
+        assertThat(s5.nthVampireNumber(5)).isEqualTo(1827);
+        assertThat(s5.nthVampireNumber(6)).isEqualTo(2187);
+        assertThat(s5.nthVampireNumber(7)).isEqualTo(6880);
+        assertThat(s5.nthVampireNumber(8)).isEqualTo(102510);
+    }
+}
